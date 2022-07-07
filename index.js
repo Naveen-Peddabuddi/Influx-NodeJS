1330d2b6804d6edc65c99b99c90fec28c7f3f7cd
Naveen Peddabuddi <nkrishvi1@gmail.com>
2022-07-07 19:41:23 +0530

    Problem statement 2 completed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.post("/items",(req,res)=> {
     }
     
     if(itemMaster.length > 0){
-        var temp = 0
+        let temp = 0
         for(let x = 0; x < itemMaster.length; x++){
             if(itemMaster[x].code === req.body.code){
                 temp = 1
@@ -36,6 +36,73 @@ app.post("/items",(req,res)=> {
 
 // Post usercart 
 
+const userCart = []
+
+app.post('/order/:orderId',(req,res)=> {
+    
+    let temp = 0;
+    if(itemMaster.length === 0){
+        return res.send("itemMaster is empty")
+    }
+    for(let x = 0; x < itemMaster.length; x++){
+        if(req.body.code === itemMaster[x].code){
+            temp = 1
+            if(userCart.length > 0){
+                let checker = 0
+                for(let i = 0; i < userCart.length; i++){
+                    if(userCart[i].orderId === req.params.orderId){
+                        checker = 1
+                        let orderobj = {
+                            "code" : req.body.code,
+                            "qty" : req.body.qty,
+                            "unitPrice" : itemMaster[x].price,
+                            "totalAmt" : itemMaster[x].price * req.body.qty
+                        }
+                        userCart[i].orders.push(orderobj)
+                        return res.send(userCart)
+                    }
+                }
+                if (checker === 0){
+                    let obj = {
+                        "orderId" : req.params.orderId,
+                        "orders" : [
+                           { 
+                            "code" : req.body.code,
+                            "qty" : req.body.qty,
+                            "unitPrice" : itemMaster[x].price,
+                            "totalAmt" : itemMaster[x].price * req.body.qty
+                            }
+                        ]
+                    }
+    
+                    userCart.push(obj)
+                    return res.send(userCart)
+                }    
+            }
+            else if(userCart.length === 0){
+                
+                let obj = {
+                    "orderId" : req.params.orderId,
+                    "orders" : [
+                       { 
+                        "code" : req.body.code,
+                        "qty" : req.body.qty,
+                        "unitPrice" : itemMaster[x].price,
+                        "totalAmt" : itemMaster[x].price * req.body.qty
+                        }
+                    ]
+                }
+
+                userCart.push(obj)
+                return res.send(userCart)
+            }
+            
+        }
+    }
+    if(temp === 0){
+       return res.status(500).send("Please enter valid code")
+    }
+})
 app.listen(8080,()=> {
     console.log("server started at port 8080")
 })
