fecaade8bc2c879645b963c3b3ad9a6c2d10002b
Naveen Peddabuddi <nkrishvi1@gmail.com>
2022-07-07 20:32:58 +0530

    completed the problem statement 3

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ const itemMaster = []
 // Post itemMaster
 
 app.post("/items",(req,res)=> {
+
+    // checking given price is number or not
     if(typeof req.body.price !== 'number'){
         res.status(500).send("Please add price to item")
     }
     
+    // validating if the code given is already present
     if(itemMaster.length > 0){
         let temp = 0
         for(let x = 0; x < itemMaster.length; x++){
@@ -26,6 +29,7 @@ app.post("/items",(req,res)=> {
             res.status(201).send(itemMaster)
         }
     }
+    // if code is unique the we are accepting the req
     else {
         itemMaster.push(req.body)
         return res.status(201).send(itemMaster)
@@ -41,6 +45,7 @@ const userCart = []
 app.post('/order/:orderId',(req,res)=> {
     
     let temp = 0;
+    // checking is the items present or not
     if(itemMaster.length === 0){
         return res.send("itemMaster is empty")
     }
@@ -101,6 +106,48 @@ app.post('/order/:orderId',(req,res)=> {
     }
     if(temp === 0){
        return res.status(500).send("Please enter valid code")
+    }
+})
+
+// GET Summarize userCart
+
+
+app.get('/order/:orderId/summarize', (req,res)=> {
+    let temp = 0
+    let summarize = []
+    
+    for(let x = 0;  x < userCart.length; x++){
+       
+        if(userCart[x].orderId === req.params.orderId){
+           
+            temp = 1
+           
+            for(let i = 0; i < userCart[x].orders.length; i++){
+                
+                if(i === 0){
+                    summarize.push(userCart[x].orders[i])      
+                }
+                else {
+                    let check = 0
+                    for(let k = 0; k < summarize.length; k++){
+                        if(summarize[k].code === userCart[x].orders[i].code){
+                            check = 1
+                            summarize[k].qty += userCart[x].orders[i].qty
+                            summarize[k].totalAmt = summarize[k].qty * summarize[k].unitPrice
+               
+                        }
+                    }
+                    if(check === 0){
+                        summarize.push(userCart[x].orders[i])
+                        
+                    }
+                }
+            }
+            return res.send(summarize)
+        }
+    }
+    if(temp === 0){
+        return res.send("Please provide valid orderId")
     }
 })
 app.listen(8080,()=> {
